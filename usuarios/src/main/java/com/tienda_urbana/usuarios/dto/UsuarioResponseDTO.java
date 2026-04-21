a74e50612e67f08d07fea4b56b93f94770f80bc2
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-04-21 08:44:25 -0400

    Creacion usuario Response DTO

---
+++ usuarios/src/main/java/com/tienda_urbana/usuarios/dto/UsuarioResponseDTO.java
@@ -0,0 +1,17 @@
+package com.tienda_urbana.usuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioResponseDTO {
+
+    private String nombre;
+    private String email;
+    private String claveRecuperacion;
+    private String fechaCreacion;
+
+}
