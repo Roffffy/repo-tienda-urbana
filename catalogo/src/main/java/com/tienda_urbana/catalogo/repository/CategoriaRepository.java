a658a8eadaf578cc78711bb46e283c2a45629514
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-04-21 20:19:16 -0400

    Creacion de repositorio para Categoria y Producto

--- catalogo/src/main/java/com/tienda_urbana/catalogo/repository/CategoriaRepository.java
+++ catalogo/src/main/java/com/tienda_urbana/catalogo/repository/CategoriaRepository.java
@@ -1,6 +1,10 @@
 package com.tienda_urbana.catalogo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tienda_urbana.catalogo.model.Categoria;
@@ -8,4 +12,6 @@ import com.tienda_urbana.catalogo.model.Categoria;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+    @Query(value = "SELECT * FROM CATEGORIA WHERE nombre LIKE CONCAT('%',:query,'%')", nativeQuery = true)
+    public List<Categoria> obtenerPorNombreParecido(@Param("query") String query);
 }
--- catalogo/src/main/java/com/tienda_urbana/catalogo/repository/ProductoRepository.java
+++ catalogo/src/main/java/com/tienda_urbana/catalogo/repository/ProductoRepository.java
@@ -1,11 +1,17 @@
 package com.tienda_urbana.catalogo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tienda_urbana.catalogo.model.Categoria;
 import com.tienda_urbana.catalogo.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    public List<Producto> findByCategoria(Optional<Categoria> categoria);
 
 }
