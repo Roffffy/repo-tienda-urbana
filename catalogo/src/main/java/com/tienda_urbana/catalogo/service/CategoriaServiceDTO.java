1260de717ff853efea7c24d4a43dd175b557f6fc
Nicolas Gutierrez <nico.gutierrezp@duocuc.cl>
2026-04-21 20:19:49 -0400

    Creacion de service para Categoria DTO

--- catalogo/src/main/java/com/tienda_urbana/catalogo/service/CategoriaServiceDTO.java
+++ catalogo/src/main/java/com/tienda_urbana/catalogo/service/CategoriaServiceDTO.java
@@ -1,6 +1,7 @@
 package com.tienda_urbana.catalogo.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import com.tienda_urbana.catalogo.dto.CategoriaRequestDTO;
 import com.tienda_urbana.catalogo.dto.CategoriaResponseDTO;
 import com.tienda_urbana.catalogo.model.Categoria;
 import com.tienda_urbana.catalogo.repository.CategoriaRepository;
+import com.tienda_urbana.catalogo.repository.ProductoRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class CategoriaServiceDTO {
 
     private final CategoriaRepository categoriaRepository;
+    private final ProductoRepository productoRepository;
 
     public CategoriaResponseDTO mapToDto(Categoria categoria) {
         return new CategoriaResponseDTO(categoria.getNombre());
@@ -24,6 +27,10 @@ public class CategoriaServiceDTO {
 
     public List<CategoriaResponseDTO> mostrarTodasCategorias() {
         return categoriaRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+    }
+
+    public List<CategoriaResponseDTO> obtenerPorNombreParecido(String query){
+        return categoriaRepository.obtenerPorNombreParecido(query).stream().map(this::mapToDto).collect(Collectors.toList());
     }
 
     public CategoriaResponseDTO agregarCategoria(CategoriaRequestDTO dto) {
@@ -39,6 +46,8 @@ public class CategoriaServiceDTO {
     }
 
     public void eliminarCategoria(Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        productoRepository.findByCategoria(categoria).forEach(producto -> producto.setCategoria(null));
         categoriaRepository.deleteById(id);
     }
 }
